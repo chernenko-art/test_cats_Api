0fbb7f11d72fbef7bac7efb4071c93c2f5fdde4a
Artem Chernenko <ac.chernenko@gmail.com>
2021-05-14 00:31:42 +0700

    Create test for check header content-type

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import pytest
 import request
 
+# Проверка, что status_code = 200
 def test_check_status_code():
     status_code, headers, fact_json = request.simple_request()
     assert status_code == 200
 
-# def test_content_type_header():
-#     status_code, headers, fact_json = request.simple_request()
-#     # headers
-#     # Проверить, что в заголовках есть ключ 'Contetn-Type' и он содержит 'application/json'
-#     content_type_header = ""
-#     # content_type_header = 'application/json'
-#     assert content_type_header == 'application/json'
+# Проверка, что в заголовках есть ключ 'Contetn-Type' и он содержит 'application/json'
+def test_content_type_header():
+    status_code, headers, fact_json = request.simple_request()
+    content_type = headers['content-type'] #обращение по ключу 'content-type'
+    header = content_type
+    for elem in content_type.split(';'): #разбиваем строку по символу "";" и итерируемся по возвращенному листу
+        if elem.strip() == 'application/json':
+            header = elem.strip()
+    assert header == 'application/json'
